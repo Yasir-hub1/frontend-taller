03c2673f0697438797b52fd22c6853edeec7f149
Soluciones Inteligentes <sinteligentesw@gmail.com>
2026-04-24 21:23:52 -0400

    config variables de entorno

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'https://ws-taller.smartcondominio.lat',
   stripePublishableKey: 'pk_test_51TLFLIFpcH4twgmZNz0sSQHb1OgPuiDb4UO9u9HvKZHCE4FbMUzNmPcIpOBmmWdngQYG1w59t4bt8Gt3plxGOsv600zmotXrY8',
   appVersion: '1.0.0',
 };
