11d59304ed6c7fc0c3ec8bc8fd7a4fb1958ac494
Soluciones Inteligentes <sinteligentesw@gmail.com>
2026-05-28 00:43:05 -0400

    change backend

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.100.231:8000',
+  apiUrl: 'https://ws-taller.smartcondominio.lat',
   stripePublishableKey: 'pk_test_51TLFLIFpcH4twgmZNz0sSQHb1OgPuiDb4UO9u9HvKZHCE4FbMUzNmPcIpOBmmWdngQYG1w59t4bt8Gt3plxGOsv600zmotXrY8',
   appVersion: '1.0.0',
   webPushPublicKey: 'BCwQLhoVRNnV8XepRvC-znzdddtGnDzEfbOLUc4kL_ANvKTz8bxBXvfCLuUIazofkkdyuzzdRjeNb-kzz9ncGSE',
